e67587bdee95b842bdb07503111187c89f664fa5
locse140456@gmail.com <locse140456@gmail.com>
2022-10-28 07:59:23 +0700

    fix verifycode error

--- BakeryRecipe/src/java/bakeryRecipe/controller/VerifyCode.java
+++ BakeryRecipe/src/java/bakeryRecipe/controller/VerifyCode.java
@@ -64,7 +64,7 @@ public class VerifyCode extends HttpServlet {
             if(code!= user.getCode()){
                 
                 //url = siteMaps.getProperty(AppContants.VerifyCodeFeatures.VERIFY_CODE_PAGE);
-                request.setAttribute("VerifyCode_ERR", "done");
+                request.setAttribute("VerifyCode_ERR", errors);
                 errors.setCodeIncorrect("This code you entered is incorrect!");
             }
         } catch (SQLException ex) {

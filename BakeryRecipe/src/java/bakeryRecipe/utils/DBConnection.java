b2915b4687c9f1872fd9b2c0f8dc2c07c89e4ebc
jdev2807 <jdev7k48kc@gmail.com>
2022-11-10 08:08:14 +0700

    Revert "Merge pull request #114 from SE1613-GROUP4-BakeryReceip/hieuvmm"
    This reverts commit 16c20ffc2fcac9f8f4b93d029f6b8070d8557571.

--- BakeryRecipe/src/java/bakeryRecipe/utils/DBConnection.java
+++ BakeryRecipe/src/java/bakeryRecipe/utils/DBConnection.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 public class DBConnection implements Serializable {    
     private static final String URL = "jdbc:mysql://localhost:3306/bakery_recipe";
     private static final String USER = "root";
-    private static final String PASSWORD = "123Bakeryrecipe123";
+    private static final String PASSWORD = "123bakeryrecipe123";
 
     
     // Function: get connection from MySQL
